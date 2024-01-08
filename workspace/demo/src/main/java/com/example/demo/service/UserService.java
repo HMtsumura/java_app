619414e2b64e1862d92bf154d2f54701b06726f8
HMtsumura <hironori0811@gmail.com>
2024-01-08 09:13:27 +0900

    first commit

---
+++ workspace/demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public void insertUser(User user){
+        User user = new User(user);
+        userRepository.save(user);
+    }
+}
